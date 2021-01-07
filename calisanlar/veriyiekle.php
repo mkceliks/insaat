342b8c72cddcdb00de885a950c4246a4611e1364
Furkan Ekici <furkanekici1997@gmail.com>
2021-01-07 20:34:56 +0300

    calisan ekle formu düzenlendi

--- calisanlar/veriyiekle.php
+++ calisanlar/veriyiekle.php
@@ -24,34 +24,19 @@
             <input class="input" type="text" name="c_isim" id="c_isim">
             
             
-            <label for="c_e_posta"><b>E-posta</b></label>
-            <input class="input" type="email" name="c_e_posta" id="c_e_posta">
-           
-            <label for="c_facebook"><b>Facebook</b></label>
-            <input class="input" type="text" name="c_facebook" id= "c_facebook">
-            
-            
-            <label for="c_twitter"><b>Twitter</b></label>
-            <input class="input" type="text" name="c_twitter" id = "c_twitter">
-            
-            
-            <label for="c_instagram"><b>Instagram</b></label>
-            <input class="input" type="text" name="c_instagram" id = "c_instagram">
             
             
             <label for="c_departman"><b>Departman</b></label>
             <input class="input" type="text" name="c_departman" id = "c_departman">
             
             
-            <label for="c_telefon"><b>Telefon</b></label>
-            <input class="input" type="text" name="c_telefon" id= "c_telefon">
             
             
-            <label for="c_adres"><b>Adres</b></label>
-            <input class="input" type="text" name="c_adres" id="c_adres">
+            <label for="yazi_baslik"><b>Yazı Başlığı</b></label>
+            <input class="input" type="text" name="yazi_baslik" id="yazi_baslik">
 
-            <label for="c_maas"><b>Maaş</b></label>
-            <input class="input" type="number" name="c_maas" id="c_maas" pattern="\d{11}">
+            <label for="yazi"><b>Yazı</b></label>
+            <textarea  type="text" name="yazi" id="yazi" cols="45" rows="7" style="resize: none;"> </textarea>
             
             <input class="input" type="submit" value="Ekle">
 
